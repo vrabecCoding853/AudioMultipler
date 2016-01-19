c852dbfe76424cefa46d0e4d64e26e2f099849dc
vrabec853 <tomasstar.enko@gmail.com>
2016-01-19 12:30:58 +0100

    commit all

---
+++ AudioMultiper/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using NAudio.Wave;
+
+namespace AudioMultiper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fileStream = File.Create("D:\\test\\test.mp3");
+            string[] inputFiles = { "D:\\test\\a.mp3", "D:\\test\\b.mp3" };
+            Combine(inputFiles,fileStream);
+            fileStream.Close();
+        }
+        public static void Combine(string[] inputFiles, Stream output)
+        {
+            foreach (string file in inputFiles)
+            {
+                Mp3FileReader reader = new Mp3FileReader(file);
+                if ((output.Position == 0) && (reader.Id3v2Tag != null))
+                {
+                    output.Write(reader.Id3v2Tag.RawData,
+                                 0,
+                                 reader.Id3v2Tag.RawData.Length);
+                }
+                Mp3Frame frame;
+                while ((frame = reader.ReadNextFrame()) != null)
+                {
+                    output.Write(frame.RawData, 0, frame.RawData.Length);
+                }
+            }
+        }
+    }
+}
+
